25b930b5985e5e984bd824d9b0fc7948325126a4
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-25 11:53:15 +0300

    First commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        anagrams = []
+        sorted_word = sorted(self.word)
+        
+        for w in words:
+            if w.lower() != self.word and sorted(w.lower()) == sorted_word:
+                anagrams.append(w)
+        
+        return anagrams
